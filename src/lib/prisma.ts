35fd2ab2cd1602c4d074bd86afd697a43e9b524b
JoSG7 <josegallodiaz3@gmail.com>
2025-02-25 12:04:17 -0500

    conexion8

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,4 +4,10 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+
+    await prisma.$disconnect()
+
+    globalForPrisma.prisma = prisma;
+
+}
