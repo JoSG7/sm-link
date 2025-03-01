77e73edc5d0780809f127ca3c3e3a65b21d06691
JoSG7 <josegallodiaz3@gmail.com>
2025-03-01 14:56:51 -0500

    design4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const HomeTitle = () => {
       <div className="relative z-10 text-center py-4">
 
         <h1 className="text-5xl font-bold bg-gradient-to-r from-amber-300 to-emerald-400 bg-clip-text tracking-tight"
-        style={{WebkitTextFillColor: "transparent"}}
+        style={{WebkitTextFillColor: "transparent", WebkitBackgroundClip: "text"}}
         >
           Link's Shortener
         </h1>
