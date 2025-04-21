d29983e30d9b943e2bfdb7d3ee16f0b421c75a79
JoSG7 <josegallodiaz3@gmail.com>
2025-04-21 11:36:16 -0500

    adding icons in the shorlinks cards

--- src/components/home/main-menu.tsx
+++ src/components/home/main-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { GitHubAuthButton, GoogleAuthButton } from "./auth-button-client"
-import { IconExternalLink, IconCopy, IconClockCheck, IconCode, IconTrash } from "@tabler/icons-react"
+import { IconExternalLink, IconCopy, IconClockCheck, IconCode, IconTrash, IconCalendar } from "@tabler/icons-react"
 import { closeMainMenu } from "@/utils/ui/home/menu-functions"
 import { getGuessLinks } from "@/logic/server-functions"
 import { LinkSkeleton } from "@/components/common/skeleton"
@@ -9,11 +9,13 @@ import { NoFound } from "@/components/common/no-found"
 import { toast } from "sonner"
 import { useState } from "react"
 import Link from "next/link"
+import { months } from "@/utils/constants"
 
 interface GuessResponse{
   id:string
   short: string,
-  original: string
+  original: string,
+  created_at: string
 }
 type GuessLinks = GuessResponse[]
 
@@ -22,27 +24,35 @@ export function MainMenu () {
   const [data, setData] = useState<GuessLinks | string>("")
   const [loading, setLoading] = useState(true)
 
-  const LinkCard = ({original, short}: {original: string, short: string}) => {
-
+  const LinkCard = ({original, short, created_at}: {original: string, short: string, created_at:string}) => {
     const url = new URL(original)
     const domain = url.hostname
+    const date = new Date(created_at)
+    const day = date.getDate()
+    const month = months[date.getMonth()]
 
     return(
 
       <article className="p-4 rounded-md border border-gray-800 whitespace-normal">
 
-        <div className="flex justify-between items-center pb-4">
-          <div className="flex flex-col max-w-[240px]">
+        <div className="flex justify-between items-center pb-4 gap-1">
+          <div className="flex flex-col max-w-[240px] sm:max-w-[285px]">
             <p className="font-semibold">sm-link.vercel.app/{short}</p>
-            <p className="w-full max-w-full text-sm text-gray-400 pt-1 break-words">{original}</p>
+            <p className="w-full max-w-full text-sm text-gray-400 mt-1 mb-2 break-words max-h-11 overflow-y-auto">{original}</p>
+            <p className="text-sm text-neutral-300 flex gap-1">
+              <IconCalendar size={20} />
+              {day} de {month}</p>
           </div>
-          <img src={`https://www.google.com/s2/favicons?domain=${domain}&sz=64`} alt="logo" className="w-12 h-12 bg-blue-500 rounded-full"/>
+          <img src={`https://www.google.com/s2/favicons?domain=${domain}&sz=64`} alt="logo" 
+          className="w-12 h-12 rounded-full sm:size-14"/>
         </div>
+
+        {/* Buttons section */}
 
         <div className="flex justify-end gap-3">
 
-          <button className="py-1 px-3 rounded-xl bg-red-700 flex gap-1 items-center text-sm" onClick={() => alert("comming soon")}>
-            Borrar
+          <button className="py-1 px-3 rounded-xl bg-red-700 flex gap-1 items-center text-sm" 
+          onClick={() => alert("comming soon")}>Borrar
             <IconTrash size={18} />
           </button>
 
@@ -53,10 +63,10 @@ export function MainMenu () {
           <button className="py-1 px-3 rounded-xl bg-[#118729] flex gap-1 items-center text-sm" 
           onClick={() => {
             navigator.clipboard.writeText(`sm-link.vercel.app/${short}`).then(() => {toast.success("Copiado Correctamente")})
-          }}>
-            Copiar
+          }}>Copiar
             <IconCopy size={18}></IconCopy>
           </button>
+
         </div>
 
       </article>
@@ -113,7 +123,7 @@ export function MainMenu () {
                 typeof data != "string" &&
 
                 data.map((element) => (
-                  <LinkCard key={element.id} short={element.short} original={element.original} />
+                  <LinkCard key={element.id} short={element.short} original={element.original} created_at={element.created_at}/>
                 ))
               }
               {
--- src/utils/ui/home/menu-functions.ts
+++ src/utils/ui/home/menu-functions.ts
@@ -55,3 +55,4 @@ export function toggleMenuOption () {
   return option
 
 }
+
