b50a8712edad0808bf0292d9f020702f63cf7e5b
JoSG7 <josegallodiaz3@gmail.com>
2025-05-27 14:51:44 -0500

    skeleton1

--- src/components/home/MainMenu.tsx
+++ src/components/home/MainMenu.tsx
@@ -8,7 +8,7 @@ import { getGuessLinks } from "@/logic/server-functions"
 import { NoFound } from "@/components/shared/NoFound"
 import { useState } from "react"
 import { LinkCard } from "../shared/LinkCard"
-import { LinkCardSkeleton } from "../shared/Skeleton"
+import { LinkCardSkeleton } from "../shared/A"
 
 
 interface GuessLinks {
