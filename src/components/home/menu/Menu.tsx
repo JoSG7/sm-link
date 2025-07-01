0952227a9ef135f42007b87d7044a92c5dd9afea
JoSG7 <josegallodiaz3@gmail.com>
2025-07-01 16:27:24 -0500

    fix1

--- src/components/home/menu/Menu.tsx
+++ src/components/home/menu/Menu.tsx
@@ -58,7 +58,7 @@ export function MainMenu() {
 
   return (
 
-    <motion.section className={`fixed bottom-0 z-20 bg-modal backdrop-blur-sm text-[#E5E7EB] 
+    <motion.section className={`fixed inset-0 z-20 bg-modal backdrop-blur-sm text-[#E5E7EB] 
       ${menu ? "pointer-events-auto" : "pointer-events-none"}`}
       onClick={toggleMenu} initial={false} animate={{ opacity: menu ? 1 : 0 }} transition={{ duration: 0.1 }} >
       <motion.nav className={`absolute bottom-0 bg-[rgb(7,7,7)] border-[#1c1c1d] overflow-y-auto ${navWidth} sm:right-0 sm:border-l`}
@@ -90,3 +90,33 @@ export function MainMenu() {
 }
 
 // #e3c45e
+
+{/* <section className="w-full h-screen fixed hidden bottom-0 bg-modal z-20 backdrop-blur-sm text-[#E5E7EB]" id="bgMenu" */}
+    // onClick={closeMainMenu}>
+    //   <nav className="w-full h-0 bg-[rgb(7,7,7)] border-[#1c1c1d] self-end overflow-y-auto duration-100 whitespace-nowrap
+    //   sm:w-0 sm:fixed sm:right-0 sm:h-full sm:border-l sm:overflow-x-hidden"
+    //   id="menu" onClick={(e) => { e.stopPropagation() }}>
+    //     <ul className="option-list flex flex-col ">
+
+    //       <Accordion items={[
+    //         {
+    //           title:
+    //             <li className="border-t border-[#1c1d1d] p-4 text-xl font-semibold flex gap-2 items-center cursor-pointer lg-2:border-none" onClick={fetchRecentLinks}>
+    //               <IconClockCheck size={24} />
+    //               Recent Sm Links
+    //             </li>,
+    //           content: <RecentLinks data={recentLinks} loading={loading} />
+    //         },
+    //         {
+    //           title:
+    //             <li className="border-t border-[#1c1d1d] p-4 text-xl font-semibold flex gap-2 items-center cursor-pointer"
+    //             onClick={() => { toggleMenu() }}>
+    //               <IconLock size={24} />
+    //               Protected Links
+    //             </li>,
+    //           content: "SISISI"
+    //         }
+    //       ]} />
+    //     </ul>
+    //   </nav>
+    // </section>
