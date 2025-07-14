c496d15cd3eaaa4a3a1f741e5af66d42e96eb7d5
JoSG7 <josegallodiaz3@gmail.com>
2025-07-14 15:54:16 -0500

    expired link message

--- src/app/[shortUrl]/page.tsx
+++ src/app/[shortUrl]/page.tsx
@@ -1,3 +1,4 @@
+import { LinkIsExpired } from "@/components/home/LinkIsExpired";
 import { PasswordForm } from "@/components/home/PasswordForm";
 import { createSupabase } from "@/lib/supabase/client";
 import { redirect } from "next/navigation";
@@ -7,6 +8,7 @@ interface Link {
   short: string
   original: string | null
   has_password: boolean
+  is_expired: boolean
 }
 
 async function ShortURL({ params }: { params: Promise<{ shortUrl: string }> }) {
@@ -14,18 +16,21 @@ async function ShortURL({ params }: { params: Promise<{ shortUrl: string }> }) {
   const supabase = createSupabase()
   const { shortUrl } = await params
   const { data, error } = await supabase.rpc("get_link_with_details", { x_short: shortUrl }).maybeSingle()
+  console.log(data)
   const link = data as Link | null
 
-  if(error){
+  if (error) {
     console.log(error)
     return <p>Has ocurred an unexpected error, please try again</p>
-  } else if(link) {
+  } else if (link) {
 
-    if(link.has_password){
+    if (link.is_expired) {
+      return <LinkIsExpired />
+    } else if (link.has_password) {
       return <PasswordForm short={shortUrl} link_id={link.id} />
     } else {
       const { error } = await supabase.rpc("record_monthly_visits", { x_link_id: link.id })
-      if(error) console.log(error)
+      if (error) console.log(error)
       redirect(link.original!)
     }
     
