d19fd5c79f2da4539dee8994e374c5bc915fc506
JoSG7 <josegallodiaz3@gmail.com>
2025-10-21 15:56:19 -0500

    Expiration modal responsive Design

--- src/features/home/components/ui/DatePicker.tsx
+++ src/features/home/components/ui/DatePicker.tsx
@@ -17,15 +17,23 @@ export function DatePicker({ value, onChange }: { value?: Date; onChange?: (date
         <Button className="w-full py-3 px-4 justify-start text-sm-movil rounded-lg border border-neutral-800 bg-neutral-950
         xs:gap-3
         sm:py-4 sm:px-5 sm:gap-4 sm:text-xl-tablet
-        md:gap-5"
+        md:gap-5
+        lg:p-3 lg:px-4 lg:gap-2 lg:text-lg-desktop
+        2xl:p-4 2xl:px-5 2xl:gap-3
+        3xl:p-5 3xl:px-6 3xl:gap-4
+        4xl:p-7 4xl:px-7 4xl:gap-6"
           size="custom">
 
-          <CalendarIcon className="size-4 xs:size-5 sm:size-6 md:size-7" />
+          <CalendarIcon className="size-4 xs:size-5 sm:size-6 md:size-7 
+          lg:size-5 2xl:size-6 3xl:size-7 4xl:size-9" />
           {date ? format(date, "PPP") : <span>Pick a date</span>}
 
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0 sm:scale-110 md:scale-125">
+      <PopoverContent className="w-auto p-0 
+      sm:scale-110 md:scale-125 lg:scale-95 
+      2xl:scale-[115%] 3xl:scale-125 
+      4xl:scale-[170%] ">
 
         <Calendar className=""
           mode="single"
@@ -42,33 +50,3 @@ export function DatePicker({ value, onChange }: { value?: Date; onChange?: (date
   )
 }
 
-
-// "use client"
-
-// import { useState } from "react"
-// import { DayPicker } from 'react-day-picker'
-// import "react-day-picker/style.css";
-
-
-// export function ExpirationCalendar({ onChange }: { onChange: (iso: Date | undefined) => void }) {
-
-//   const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
-
-//   const handleSelected = (newDay: Date) => {
-//     setSelectedDate(newDay)
-//     onChange(newDay)
-//   }
-
-//   return (
-//     <div className="flex justify-center">
-//       <DayPicker mode="single" disabled={{ before: new Date() }} animate required selected={selectedDate}
-//       className="scale-90 xs:scale-100 sm:scale-110 md:scale-110"
-//       onSelect={handleSelected}
-//       classNames={{
-//         today: "text-sky-500",
-//         selected: "bg-sky-500 rounded-full duration-500 text-white",
-//       }} />
-//     </div>
-//   )
-
-// }
